9d4ae9539381ae2d28daccd3e75c3df2d5c8db74
Murilo Sanches <murilando@gmail.com>
2020-02-15 18:02:50 -0300

    Testes do Handler que faltava

--- Crmall.Tests/HandlerTest/ClienteHandlerTest.cs
+++ Crmall.Tests/HandlerTest/ClienteHandlerTest.cs
@@ -11,19 +11,18 @@ namespace Crmall.Tests.HandlerTest
 {
     public class ClienteHandlerTest
     {
+        private Mock<IClienteRepository> _mockRepositorio = new Mock<IClienteRepository>();
+        private Mock<IUnitOfWork> _mockUnitOfWork = new Mock<IUnitOfWork>();
+
         [Fact]
         public void Dado_Um_NovoClienteCommand_Invalido_Deve_Retornar_Sucesso_False()
         {
             //Arrange
-            var mockRepositorio = new Mock<IClienteRepository>();
-            var mockUnitOfWork = new Mock<IUnitOfWork>();
-
-            var novoComandoClienteInvalido = new NovoClienteCommand("", DateTime.Now.AddYears(-200), ESexo.Feminino);
-            var clienteHandler = new ClienteHandler(mockRepositorio.Object, mockUnitOfWork.Object);
+            var novoClienteComandoClienteInvalido = new NovoClienteCommand("", DateTime.Now.AddYears(-200), ESexo.Feminino);
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
 
             //Act
-            var resultado  = (ResultadoGenerico) clienteHandler.Executar(novoComandoClienteInvalido);
-
+            var resultado  = (ResultadoGenerico) clienteHandler.Executar(novoClienteComandoClienteInvalido);
 
             Assert.False(resultado.Sucesso);
         }
@@ -31,31 +30,82 @@ namespace Crmall.Tests.HandlerTest
         [Fact]
         public void Dado_Um_NovoClienteCommand_Valido_Deve_Retornar_Sucesso_True()
         {
-            Assert.False(true);
+            //Arrange
+            var novoClienteComandoClienteValido = new NovoClienteCommand("Murilo Sanches", DateTime.Now, ESexo.Feminino);
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(novoClienteComandoClienteValido);
+
+            Assert.True(resultado.Sucesso);
         }
 
         [Fact]
         public void Dado_Um_AtualizarClienteCommand_Invalido_Deve_Retornar_Sucesso_False()
         {
-            Assert.False(true);
+            //Arrange
+            var atualizarClienteCommandInvalido = new AtualizarClienteCommand(Guid.Empty, "Murilo Sanches", DateTime.Now, ESexo.Feminino);
+            _mockRepositorio.Setup(x => x.BuscarPorId(It.IsAny<Guid>())).Returns(It.IsAny<Cliente>());
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(atualizarClienteCommandInvalido);
+
+            Assert.False(resultado.Sucesso);
         }
 
         [Fact]
         public void Dado_Um_AtualizarClienteCommmand_Valido_Deve_Retornar_Sucesso_True()
         {
-            Assert.False(true);
+            //Arrange
+            var atualizarClienteCommandValido = new AtualizarClienteCommand(Guid.NewGuid(), "Murilo Sanches", DateTime.Now, ESexo.Feminino);
+            _mockRepositorio.Setup(x => x.BuscarPorId(It.IsAny<Guid>())).Returns(new Cliente(Guid.NewGuid(), "Murilo Sanches", DateTime.Now, ESexo.Masculino, null));
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(atualizarClienteCommandValido);
+
+            Assert.True(resultado.Sucesso);
+        }
+
+        [Fact]
+        public void Dado_Um_AtualizarClienteCommand_Valido_Mas_Com_Um_Id_Nao_Encontrado_No_Banco_Deve_Retornar_Sucesso_False()
+        {
+            //Arrange
+            var atualizarClienteCommandValido = new AtualizarClienteCommand(Guid.NewGuid(), "Murilo Sanches", DateTime.Now, ESexo.Feminino);
+            _mockRepositorio.Setup(x => x.BuscarPorId(It.IsAny<Guid>())).Returns<Cliente>(null);
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(atualizarClienteCommandValido);
+
+            Assert.False(resultado.Sucesso);
         }
 
         [Fact]
         public void Dado_Um_RemoverClienteCommand_Invalido_Deve_Retornar_Sucesso_False()
         {
-            Assert.False(true);
+            //Arrange
+            var removerClienteCommandInvalido = new RemoverClienteCommand(Guid.Empty);
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(removerClienteCommandInvalido);
+
+            Assert.False(resultado.Sucesso);
         }
 
         [Fact]
         public void Dado_Um_RemoverClienteCommmand_Valido_Deve_Retornar_Sucesso_True()
         {
-            Assert.False(true);
+            //Arrange
+            var removerClienteCommandValido = new RemoverClienteCommand(Guid.NewGuid());
+            var clienteHandler = new ClienteHandler(_mockRepositorio.Object, _mockUnitOfWork.Object);
+
+            //Act
+            var resultado = (ResultadoGenerico)clienteHandler.Executar(removerClienteCommandValido);
+
+            Assert.True(resultado.Sucesso);
         }
     }
 }
